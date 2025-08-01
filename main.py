3678204e0c4e7ea9aa7ac956537f69bfe9258334
popovvladimir12021992-bit <popov.vladimir12021992@gmail.com>
2025-08-01 13:40:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -361,3 +361,13 @@ def main():
 
 if __name__ == "__main__":
     main()
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "I'm alive!"
+def run():
+    app.run(host='0.0.0.0', port=8080)
+Thread(target=run).start()
